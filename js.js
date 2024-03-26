89ebedb1c3159ea2886b298dc92583abe1d36c6b
BurcisWolf <98662129+BurcisWolf@users.noreply.github.com>
2024-03-26 01:10:20 +0100

    Made basic Javascript for article window, + and - button which can open up or close the article, still a very basic approach to it

---
+++ js.js
@@ -0,0 +1,65 @@
+function menu(){
+    if(document.querySelector(".article .clanek").classList.contains("open")){
+        document.querySelector(".article .clanek").classList.remove("open");
+        document.querySelector(".article .clanek").classList.add("hide");
+        document.querySelector(".fa-solid.icon1").classList.remove("fa-minus");
+        document.querySelector(".fa-solid.icon1").classList.add("fa-plus");
+    } else {
+        document.querySelector(".article .clanek").classList.remove("hide");
+        document.querySelector(".article .clanek").classList.add("open");
+        document.querySelector(".fa-solid.icon1").classList.remove("fa-plus");
+        document.querySelector(".fa-solid.icon1").classList.add("fa-minus");
+    }
+}
+function menu2(){
+    if(document.querySelector(".article .clanek2").classList.contains("open")){
+        document.querySelector(".article .clanek2").classList.remove("open");
+        document.querySelector(".article .clanek2").classList.add("hide");
+        document.querySelector(".fa-solid.icon2").classList.remove("fa-minus");
+        document.querySelector(".fa-solid.icon2").classList.add("fa-plus");
+    } else {
+        document.querySelector(".article .clanek2").classList.remove("hide");
+        document.querySelector(".article .clanek2").classList.add("open");
+        document.querySelector(".fa-solid.icon2").classList.remove("fa-plus");
+        document.querySelector(".fa-solid.icon2").classList.add("fa-minus");
+    }
+}
+function menu3(){
+    if(document.querySelector(".article .clanek3").classList.contains("open")){
+        document.querySelector(".article .clanek3").classList.remove("open");
+        document.querySelector(".article .clanek3").classList.add("hide");
+        document.querySelector(".fa-solid.icon3").classList.remove("fa-minus");
+        document.querySelector(".fa-solid.icon3").classList.add("fa-plus");
+    } else {
+        document.querySelector(".article .clanek3").classList.remove("hide");
+        document.querySelector(".article .clanek3").classList.add("open");
+        document.querySelector(".fa-solid.icon3").classList.remove("fa-plus");
+        document.querySelector(".fa-solid.icon3").classList.add("fa-minus");
+    }
+}
+function menu4(){
+    if(document.querySelector(".article .clanek4").classList.contains("open")){
+        document.querySelector(".article .clanek4").classList.remove("open");
+        document.querySelector(".article .clanek4").classList.add("hide");
+        document.querySelector(".fa-solid.icon4").classList.remove("fa-minus");
+        document.querySelector(".fa-solid.icon4").classList.add("fa-plus");
+    } else {
+        document.querySelector(".article .clanek4").classList.remove("hide");
+        document.querySelector(".article .clanek4").classList.add("open");
+        document.querySelector(".fa-solid.icon4").classList.remove("fa-plus");
+        document.querySelector(".fa-solid.icon4").classList.add("fa-minus");
+    }
+}
+function menu5(){
+    if(document.querySelector(".article .clanek5").classList.contains("open")){
+        document.querySelector(".article .clanek5").classList.remove("open");
+        document.querySelector(".article .clanek5").classList.add("hide");
+        document.querySelector(".fa-solid.icon5").classList.remove("fa-minus");
+        document.querySelector(".fa-solid.icon5").classList.add("fa-plus");
+    } else {
+        document.querySelector(".article .clanek5").classList.remove("hide");
+        document.querySelector(".article .clanek5").classList.add("open");
+        document.querySelector(".fa-solid.icon5").classList.remove("fa-plus");
+        document.querySelector(".fa-solid.icon5").classList.add("fa-minus");
+    }
+}
