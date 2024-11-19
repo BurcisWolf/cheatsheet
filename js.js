cd6894824b673e0d132f6daeab77357bae59277d
BurcisWolf <98662129+BurcisWolf@users.noreply.github.com>
2024-11-19 21:33:45 +0100

    Rewrote whole JavaScript function to not be so long, added some HTML and CSS styling for future user

--- js.js
+++ js.js
@@ -1,94 +1,13 @@
-
-// NEED COMPLETE REWORK, I DONT LIKE IT THIS WAY!!!!
-
-function menu(){
-    if(document.querySelector(".article .clanek").classList.contains("open")){
-        document.querySelector(".article .clanek").classList.remove("open");
-        document.querySelector(".article .clanek").classList.add("hide");
-        document.querySelector(".fa-solid.icon1").classList.remove("fa-minus");
-        document.querySelector(".fa-solid.icon1").classList.add("fa-plus");
+function menu(number){
+    if(document.querySelector(".article .clanek" + number).classList.contains("open")){
+        document.querySelector(".article .clanek" + number).classList.remove("open");
+        document.querySelector(".article .clanek" + number).classList.add("hide");
+        document.querySelector(".fa-solid.icon" + number).classList.remove("fa-minus");
+        document.querySelector(".fa-solid.icon" + number).classList.add("fa-plus");
     } else {
-        document.querySelector(".article .clanek").classList.remove("hide");
-        document.querySelector(".article .clanek").classList.add("open");
-        document.querySelector(".fa-solid.icon1").classList.remove("fa-plus");
-        document.querySelector(".fa-solid.icon1").classList.add("fa-minus");
-    }
-}
-function menu2(){
-    if(document.querySelector(".article .clanek2").classList.contains("open")){
-        document.querySelector(".article .clanek2").classList.remove("open");
-        document.querySelector(".article .clanek2").classList.add("hide");
-        document.querySelector(".fa-solid.icon2").classList.remove("fa-minus");
-        document.querySelector(".fa-solid.icon2").classList.add("fa-plus");
-    } else {
-        document.querySelector(".article .clanek2").classList.remove("hide");
-        document.querySelector(".article .clanek2").classList.add("open");
-        document.querySelector(".fa-solid.icon2").classList.remove("fa-plus");
-        document.querySelector(".fa-solid.icon2").classList.add("fa-minus");
-    }
-}
-function menu3(){
-    if(document.querySelector(".article .clanek3").classList.contains("open")){
-        document.querySelector(".article .clanek3").classList.remove("open");
-        document.querySelector(".article .clanek3").classList.add("hide");
-        document.querySelector(".fa-solid.icon3").classList.remove("fa-minus");
-        document.querySelector(".fa-solid.icon3").classList.add("fa-plus");
-    } else {
-        document.querySelector(".article .clanek3").classList.remove("hide");
-        document.querySelector(".article .clanek3").classList.add("open");
-        document.querySelector(".fa-solid.icon3").classList.remove("fa-plus");
-        document.querySelector(".fa-solid.icon3").classList.add("fa-minus");
-    }
-}
-function menu4(){
-    if(document.querySelector(".article .clanek4").classList.contains("open")){
-        document.querySelector(".article .clanek4").classList.remove("open");
-        document.querySelector(".article .clanek4").classList.add("hide");
-        document.querySelector(".fa-solid.icon4").classList.remove("fa-minus");
-        document.querySelector(".fa-solid.icon4").classList.add("fa-plus");
-    } else {
-        document.querySelector(".article .clanek4").classList.remove("hide");
-        document.querySelector(".article .clanek4").classList.add("open");
-        document.querySelector(".fa-solid.icon4").classList.remove("fa-plus");
-        document.querySelector(".fa-solid.icon4").classList.add("fa-minus");
-    }
-}
-function menu5(){
-    if(document.querySelector(".article .clanek5").classList.contains("open")){
-        document.querySelector(".article .clanek5").classList.remove("open");
-        document.querySelector(".article .clanek5").classList.add("hide");
-        document.querySelector(".fa-solid.icon5").classList.remove("fa-minus");
-        document.querySelector(".fa-solid.icon5").classList.add("fa-plus");
-    } else {
-        document.querySelector(".article .clanek5").classList.remove("hide");
-        document.querySelector(".article .clanek5").classList.add("open");
-        document.querySelector(".fa-solid.icon5").classList.remove("fa-plus");
-        document.querySelector(".fa-solid.icon5").classList.add("fa-minus");
-    }
-}
-function menu6(){
-    if(document.querySelector(".article .clanek6").classList.contains("open")){
-        document.querySelector(".article .clanek6").classList.remove("open");
-        document.querySelector(".article .clanek6").classList.add("hide");
-        document.querySelector(".fa-solid.icon6").classList.remove("fa-minus");
-        document.querySelector(".fa-solid.icon6").classList.add("fa-plus");
-    } else {
-        document.querySelector(".article .clanek6").classList.remove("hide");
-        document.querySelector(".article .clanek6").classList.add("open");
-        document.querySelector(".fa-solid.icon6").classList.remove("fa-plus");
-        document.querySelector(".fa-solid.icon6").classList.add("fa-minus");
-    }
-}
-function menu7(){
-    if(document.querySelector(".article .clanek7").classList.contains("open")){
-        document.querySelector(".article .clanek7").classList.remove("open");
-        document.querySelector(".article .clanek7").classList.add("hide");
-        document.querySelector(".fa-solid.icon7").classList.remove("fa-minus");
-        document.querySelector(".fa-solid.icon7").classList.add("fa-plus");
-    } else {
-        document.querySelector(".article .clanek7").classList.remove("hide");
-        document.querySelector(".article .clanek7").classList.add("open");
-        document.querySelector(".fa-solid.icon7").classList.remove("fa-plus");
-        document.querySelector(".fa-solid.icon7").classList.add("fa-minus");
+        document.querySelector(".article .clanek" + number).classList.remove("hide");
+        document.querySelector(".article .clanek" + number).classList.add("open");
+        document.querySelector(".fa-solid.icon" + number).classList.remove("fa-plus");
+        document.querySelector(".fa-solid.icon" + number).classList.add("fa-minus");
     }
 }
